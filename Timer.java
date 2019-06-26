125d99f037169d0cdc8277e87da655995fe835e2
Kraftex <kamilo1704@gmail.com>
2019-06-26 20:16:15 +0200

    Add: Timer.java

---
+++ Timer.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+public class Timer
+{
+    private long start;
+
+    public Timer ( )
+    {
+        start = System.currentTimeMillis( );
+    }
+
+    public void restart ( )
+    {
+        start = System.currentTimeMilis( );
+    }
+
+    public boolean isElaspeTimeHours ( int h )
+    {
+        return h <= getElaspeTime( )/(3600*1000);
+    }
+    public boolean isElaspeTimeMinites ( int m )
+    {
+        return m <= getElaspeTime( )/(60*1000);
+    }
+    public boolean isElaspeTimeSeconds ( int s )
+    {
+        return s <= getElaspeTime( )/(1000);
+    }
+    public boolean isElaspeTime ( int hours, int minites, int seconds )
+    {
+        return isElaspeTimeHours(hours)
+            && isElaspeTimeMinites(minites)
+            && isElaspeTimeSeconds(seconds);
+    }
+    public long getElaspeTime ( )
+    {
+        return actualTime( ) - start;
+    }
+    public long actualTime ( )
+    {
+        return System.currentTimeMillis( );
+    }
+    public String toString ( )
+    {
+        Date that = new Date(beginingTasks);
+        return that.getHours( ) + ":" + that.getMinutes( ) + ":" + that.getSeconds( );
+    }
+}
